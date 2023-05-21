817b1ea23fb8fb2abc9f1c2499bfee8d4b320d3d
Pankaj Kumar <121335947+pkthapliyal@users.noreply.github.com>
2023-05-21 23:57:40 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 const mongoURL = process.env.mongoURL;
-const connection = mongoose.connect(mongoURL);
+const connection = mongoose.connect("mongodb+srv://pkthapliyal:pankajkr@cluster0.l1f5yob.mongodb.net/RBAC?retryWrites=true&w=majority");
 
 
 module.exports = { connection }
